86eae991b5cb7f72b68a2f3d0b938363ad2cae4d
ShivPatel031 <patelshiv3123@gmail.com>
2024-05-20 15:28:30 +0530

    code readability

--- WEB/ToDoList/ToDoList.js
+++ WEB/ToDoList/ToDoList.js
@@ -1,8 +1,14 @@
+// get necessary tag
 let userinput = document.getElementById('userinput');
 let addaction = document.getElementById('addaction');
 let addcontainer = document.getElementsByClassName('addcontainer')[0];
+
+//load data
 var ld=JSON.parse(localStorage.getItem('ld'));
+
+//check if data exist or not if not then create
 if (ld == null){
+
     let localData = {data:[]};
     let jsonstring = JSON.stringify(localData);
     localStorage.setItem("ld",jsonstring);
@@ -10,57 +16,70 @@ if (ld == null){
 }
 // localStorage.removeItem('ld');
 
+//load data
 function loadData(){
     
     for ( let value of ld.data){
         additems(value);
-    }
-    
+    } 
 }
 loadData();
 
+//add in list container
 function additems(value){
+
     let lit = document.createElement('li');
     let divt = document.createElement('div');
     let pt = document.createElement('p');
     let buttont = document.createElement('button');
+
     pt.innerText=value;
+
     buttont.innerText="Delete";
     buttont.classList.add("deletebutton");
+
     divt.appendChild(pt);
     divt.appendChild(buttont);
-
     divt.classList.add('ToDoitems');
+
     lit.appendChild(divt);
+
     addcontainer.append(lit);
 
 }
 
+
+//store data
 function saveData(ld){
+
     let jsonstring = JSON.stringify(ld);
     localStorage.setItem("ld",jsonstring);
+
 }
 
-
+//add button event
 addaction.addEventListener("click",()=>{
-    ld.data.push(userinput.value);
-   additems(userinput.value);
-    userinput.value="";
-    saveData(ld);
-   
-    
 
+    ld.data.push(userinput.value);
+
+    additems(userinput.value);
+
+    userinput.value="";
+
+    saveData(ld);
    
 });
 
+//delete button event
 addcontainer.addEventListener('click',(event)=>{
+
     let i =ld.data.indexOf(event.target.previousSibling.innerText);
     ld.data.splice(i,1);
+
     saveData(ld);
+
     event.target.parentElement.parentElement.remove();
 
+});
 
-})
 
-
-//add in storage
