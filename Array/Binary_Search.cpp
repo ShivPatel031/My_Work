37b67037f2ae0d488ba24329680ff68048c7be35
ShivPatel031 <113342110+ShivPatel031@users.noreply.github.com>
2024-06-08 10:02:58 +0530

    Binary Search

---
+++ Array/Binary_Search.cpp
@@ -0,0 +1,25 @@
+int search(vector<int>& nums, int target) {
+
+        int l = 0;
+        int r = nums.size()-1;
+
+       
+        while(l<=r){
+            int m = (l + r)/2;
+            cout<<r;
+            if (nums[m] == target){
+                return m;
+            }
+            else if( target > nums[m]){
+                l=m+1;
+            }
+            else{
+
+                r=m-1;
+            }
+        }
+        
+
+        return -1;
+        
+}
