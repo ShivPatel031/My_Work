164a52a3fca95a7f3b73d02ee90ddb75bd9458a7
ShivPatel031 <113342110+ShivPatel031@users.noreply.github.com>
2024-06-08 08:30:42 +0530

    Bubble Sort

---
+++ Array/Sorts/Bubble_Sort.cpp
@@ -0,0 +1,30 @@
+
+
+vector<int> Bubble_sort(vector<int> array)
+{
+
+    bool check = true;
+
+    for (int i = 0; i < array.size() - 1; i++)
+    {
+        for (int j = 0; j < array.size() - 1 - i; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+
+                check = false;
+            }
+        }
+
+        if (check)
+        {
+            break;
+        }
+    }
+
+    return array;
+}
