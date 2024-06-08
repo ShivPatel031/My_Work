9598da07f53037c08de039f898a27cdeb24697e2
ShivPatel031 <113342110+ShivPatel031@users.noreply.github.com>
2024-06-08 18:51:21 +0200

    Search in Rotated Sorted Array

---
+++ Array/Binary_Search_Problems/Search_in_Rotated_Sorted_Array.cpp
@@ -0,0 +1,52 @@
+// Search in Rotated Sorted Array
+
+// Example 1:
+
+// Input: nums = [4,5,6,7,0,1,2], target = 0
+// Output: 4
+// Example 2:
+
+// Input: nums = [4,5,6,7,0,1,2], target = 3
+// Output: -1
+// Example 3:
+
+// Input: nums = [1], target = 0
+// Output: -1
+
+int search(vector<int>& arr, int target) {
+
+        int l = 0 , r =arr.size()-1;
+        int n = r;
+
+        while (l<=r){
+            int m = l + (r-l)/2;
+
+            if(arr[m]==target){
+                return m;
+            }
+            else if(arr[m]>=arr[l] && arr[m]>arr[r]){
+                if ((arr[m]>target && arr[0]>target) || (target > arr[0] && target > arr[m])){
+                    l=m+1;
+                }else{
+                    r=m-1;
+                }
+            }
+            else if(arr[m]<arr[l] && arr[m]<arr[r]){
+                if(target <= arr[n] && target >arr[m]){
+                    l=m+1;
+                }else{
+                    r=m-1;
+                }
+            }
+            else{
+                if(arr[m]<target){
+                    l=m+1;
+                }
+                else{
+                    r=m-1;
+                }
+            }
+        }
+        return -1;
+        
+    }
