0dc1a4fe739f420256ae14f3a97e4380338d2b67
ShivPatel031 <113342110+ShivPatel031@users.noreply.github.com>
2024-06-14 09:27:45 +0530

    Transpose of Matrix

--- Array/Matrix/Transpose_of_Matrix.cpp
+++ Array/Matrix/Transpose_of_Matrix.cpp
@@ -1,5 +1,7 @@
 // Transpose of Matrix
 
+// GFG
+
 // Write a program to find the transpose of a square matrix of size N*N. Transpose of a matrix is obtained by changing rows to columns and columns to rows.
 
 // Example 1:
@@ -36,4 +38,33 @@ void transpose(vector<vector<int> >& matrix, int n)
                 matrix[j][i]=temp;
             }
         } 
+    }
+
+
+// Leetcode 867
+
+// Given a 2D integer array matrix, return the transpose of matrix.
+
+// The transpose of a matrix is the matrix flipped over its main diagonal, switching the matrix's row and column indices.
+
+// Example 1:
+
+// Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+// Output: [[1,4,7],[2,5,8],[3,6,9]]
+    
+// Example 2:
+
+// Input: matrix = [[1,2,3],[4,5,6]]
+// Output: [[1,4],[2,5],[3,6]]
+
+ vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        vector<vector<int>> result(matrix[0].size(),vector<int>(matrix.size()));
+
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix[0].size();j++){
+                result[j][i]=matrix[i][j];
+            }
+        }
+
+        return result;
     }
