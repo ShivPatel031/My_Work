cc4480bf1b11edd30586a9b4cc9259a621f29f69
ShivPatel031 <113342110+ShivPatel031@users.noreply.github.com>
2024-06-14 09:51:47 +0530

    Toeplitz Matrix

---
+++ Array/Matrix/Toeplitz_Matrix.cpp
@@ -0,0 +1,37 @@
+// Toeplitz Matrix leet code 766
+
+// Given an m x n matrix, return true if the matrix is Toeplitz. Otherwise, return false.
+
+// A matrix is Toeplitz if every diagonal from top-left to bottom-right has the same elements.
+
+// Example 1:
+
+
+// Input: matrix = [[1,2,3,4],[5,1,2,3],[9,5,1,2]]
+// Output: true
+// Explanation:
+// In the above grid, the diagonals are:
+// "[9]", "[5, 5]", "[1, 1, 1]", "[2, 2, 2]", "[3, 3]", "[4]".
+// In each diagonal all elements are the same, so the answer is True.
+  
+// Example 2:
+
+
+// Input: matrix = [[1,2],[2,2]]
+// Output: false
+// Explanation:
+// The diagonal "[1, 2]" has different elements.
+
+
+bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+
+        for (int i = 1; i < matrix.size(); i++) {
+            for (int j = 1; j < matrix[0].size(); j++) {
+                if (matrix[i][j] != matrix[i - 1][j - 1]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
