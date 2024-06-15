6798f28d2bb405ed14ea63411321e9f8e17cd6e1
ShivPatel031 <113342110+ShivPatel031@users.noreply.github.com>
2024-06-15 08:50:09 +0530

    Search in a row-column sorted Matrix

---
+++ Array/Matrix/Search_in_a_row-column_sorted_Matrix.cpp
@@ -0,0 +1,60 @@
+// Search in a row-column sorted Matrix
+
+// Given a matrix of size n x m, where every row and column is sorted in increasing order, and a number x. Find whether element x is present in the matrix or not.
+
+// Example 1:
+
+// Input:
+// n = 3, m = 3, x = 62
+// matrix[][] = {{ 3, 30, 38},
+//               {36, 43, 60},
+//               {40, 51, 69}}
+// Output: 0
+// Explanation:
+// 62 is not present in the matrix, 
+// so output is 0.
+  
+// Example 2:
+
+// Input:
+// n = 1, m = 6, x = 55
+// matrix[][] = {{18, 21, 27, 38, 55, 67}}
+// Output: 1
+// Explanation: 55 is present in the matrix.
+
+bool search(vector<vector<int> > matrix, int n, int m, int target) 
+    {
+        int index=0;
+
+        while(index<n){
+            
+            if(matrix[index][0] <= target && target <= matrix[index][m-1]){
+                
+                int start=0;
+                int end=m-1;
+    
+                while(start<=end){
+                    
+                    int mid = start + (end-start)/2;
+                                 
+                    if(matrix[index][mid] == target)
+                    {
+                        return 1;
+                    }
+                    else if( matrix[index][mid]<target)
+                    {
+                        start=mid+1;
+                    }
+                    else
+                    {
+                        end=mid-1;
+                    }
+                }
+            
+            }
+            
+            index++;
+        }
+
+        return 0; 
+    }
