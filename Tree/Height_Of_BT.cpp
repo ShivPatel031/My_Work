5851fc863ca12df676f70045ec0afa8bda02f4b1
ShivPatel031 <patelshiv3123@gmail.com>
2024-06-17 15:30:32 +0530

    change

--- Tree/Height_Of_BT.cpp
+++
@@ -1,7 +0,0 @@
-//height of tree means total level of any tree
-int heightOfbinaryTreeRecursive(Node *root){
-        if (!root){
-            return 0;
-        }
-        return 1 + max(heightOfbinaryTreerecursive(root->left),heightOfbinaryTreerecursive(root->right));
-    }
