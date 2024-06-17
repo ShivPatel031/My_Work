315a54a5e6647c1bd977c8f8222863c0ca9e183a
ShivPatel031 <113342110+ShivPatel031@users.noreply.github.com>
2024-06-17 07:36:38 +0530

    Defanging an IP Address

---
+++ Strings/Defanging_an_IP_Address.cpp
@@ -0,0 +1,32 @@
+// Defanging an IP Address leetcode 1108
+
+// Given a valid (IPv4) IP address, return a defanged version of that IP address.
+
+// A defanged IP address replaces every period "." with "[.]".
+
+ 
+
+// Example 1:
+
+// Input: address = "1.1.1.1"
+// Output: "1[.]1[.]1[.]1"
+
+// Example 2:
+
+// Input: address = "255.100.50.0"
+// Output: "255[.]100[.]50[.]0"
+
+string defangIPaddr(string address) {
+
+        string ans;
+
+        for(int i = 0;i<address.size();i++){
+            if(address[i]=='.'){
+                ans+="[.]";
+            }else{
+                ans+=address[i];
+            }
+        } 
+
+        return ans;  
+    }
